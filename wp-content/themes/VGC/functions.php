82d4525e3e837e1469a425ca42cdc04f11706c21
lamquoctuan <lamquoctuan@gmail.com>
2015-03-21 10:01:04 -0500

    apply search form and search results

--- wp-content/themes/VGC/functions.php
+++ wp-content/themes/VGC/functions.php
@@ -9,6 +9,59 @@
 if ( !defined('TEMP_DIR') )
     define('TEMP_DIR', get_template_directory_uri());
 
+if ( ! function_exists( 'vgc_setup' ) ) :
+/**
+ * Make theme available for translation.
+ */
+function vgc_setup()
+{
+    /**
+     * Add default posts and comments RSS feed links to head.
+     */
+    add_theme_support( 'automatic-feed-links' );
+
+    /**
+     * Let WordPress manage the document title.
+     */
+    add_theme_support( 'title-tag' );
+
+    /**
+     * Enable support for Post Thumbnails on posts and pages.
+     */
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 825, 510, true );
+
+    // This theme uses wp_nav_menu() in five locations.
+    register_nav_menus( array(
+        'top-nav-right' => __( 'Top Nav Right Menu',    'vgc' ),
+        'left-side'     => __( 'Left Menu',             'vgc' ),
+        'primary'       => __( 'Primary Menu',          'vgc' ),
+        'social'        => __( 'Social Links Menu',     'vgc' ),
+        'payment'        => __( 'Payment Menu',         'vgc' ),
+    ) );
+
+    /*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+    add_theme_support( 'html5', array(
+        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+    ) );
+
+    /*
+	 * Enable support for Post Formats.
+	 *
+	 * See: https://codex.wordpress.org/Post_Formats
+	 */
+    add_theme_support( 'post-formats', array(
+        'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+    ) );
+
+    add_editor_style( array( 'css/editor-style.css', 'css/style.css', 'css/styles.css', vgc_fonts_url() ) );
+}
+endif; // vgc_setup
+add_action( 'after_setup_theme', 'vgc_setup' );
+
 if ( ! function_exists( 'vgc_fonts_url' ) ) :
     /**
      * Register Google fonts for VGC.
@@ -61,91 +114,91 @@ if ( ! function_exists( 'vgc_fonts_url' ) ) :
     }
 endif;
 
+if ( ! function_exists( 'vgc_enqueue_styles' ) ) :
+    /**
+     * Enqueue styles.
+     *
+     * @since VGC 1.0
+     */
+    function vgc_enqueue_styles() {
+        // Add custom fonts, used in the main stylesheet.
+        wp_enqueue_style( 'vgc-fonts', vgc_fonts_url(), array(), null );
+
+        // Add ..., used in the main stylesheet.
+        // Load our main stylesheet.
+        wp_enqueue_style( 'vgc-core', get_stylesheet_uri(), false );
+        wp_enqueue_style( 'vgc-bootstrap', TEMP_DIR . '/css/bootstrap.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-styles', TEMP_DIR . '/css/styles.css', array( 'vgc-core' ), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-style', TEMP_DIR . '/css/style.css', array( 'vgc-core' ), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-bootstrap-select.min', TEMP_DIR . '/css/bootstrap-select.min.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-bootstrap-theme', TEMP_DIR . '/css/bootstrap-theme.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-widgets', TEMP_DIR . '/css/widgets.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-custommenu', TEMP_DIR . '/css/custommenu.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-effect', TEMP_DIR . '/css/effect.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-ma.onsaleslider', TEMP_DIR . '/css/ma.onsaleslider.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-categorytabs', TEMP_DIR . '/css/categorytabs.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-categorytabsliders', TEMP_DIR . '/css/categorytabsliders.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-searchbycategory', TEMP_DIR . '/css/searchbycategory.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-ma.cltool', TEMP_DIR . '/css/ma.cltool.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-ma.upsellslider', TEMP_DIR . '/css/ma.upsellslider.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-leftvmegamenu', TEMP_DIR . '/css/leftvmegamenu.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-ma.banner8', TEMP_DIR . '/css/ma.banner8.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-bannersequence', TEMP_DIR . '/css/bannersequence.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-ma.newslider', TEMP_DIR . '/css/ma.newslider.css', array(), '20150320' , 'all');
+        wp_enqueue_style( 'vgc-ajax_cart_super', TEMP_DIR . '/css/ajax_cart_super.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-jquery-ui', TEMP_DIR . '/css/jquery-ui.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-jquery.fancybox', TEMP_DIR . '/css/jquery.fancybox.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-bestsellervertscroller', TEMP_DIR . '/css/bestsellervertscroller.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-ma.brandslider.css', TEMP_DIR . '/css/ma.brandslider.css', array(), '20150320' , 'all');
+    //    wp_enqueue_style( 'vgc-print.css', TEMP_DIR . '/css/print.css', array(), '20150320' , 'print');
+    }
+endif;
+
+if ( ! function_exists( 'vgc_enqueue_scripts' ) ) :
+    /**
+     * Enqueue scripts.
+     *
+     * @since VGC 1.0
+     */
+    function vgc_enqueue_scripts() {
+        wp_enqueue_script( 'vgc-jquery-js', '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js', array(), '20150320', false );
+        wp_enqueue_script( 'vgc-bootstrap', TEMP_DIR . '/js/bootstrap.js', array(), '20150320', false );
+        wp_enqueue_script( 'vgc-prototype-js', TEMP_DIR . '/js/prototype.js', array('vgc-jquery-js'), '20150320', false );
+        wp_enqueue_script( 'vgc-validation-js', TEMP_DIR . '/js/validation.js', array(), '20150320', false );
+        wp_enqueue_script( 'vgc-effects-js', TEMP_DIR . '/js/effects.js', array(), '20150320', false );
+        wp_enqueue_script( 'vgc-controls-js', TEMP_DIR . '/js/controls.js', array('vgc-prototype-js','vgc-effects-js'), '20150320', false );
+        wp_enqueue_script( 'vgc-js-js', TEMP_DIR . '/js/js.js', array('vgc-prototype-js', 'vgc-validation-js', 'vgc-controls-js'), '20150320', false );
+        wp_enqueue_script( 'vgc-slide-js', TEMP_DIR . '/js/ma.jq.slide.js', array(), '20150320', false );
+        wp_enqueue_script( 'vgc-flexslider-js', TEMP_DIR . '/js/ma.flexslider.js', array(), '20150320', false );
+        wp_enqueue_script( 'vgc-sequence-js', TEMP_DIR . '/js/jquery.sequence-min.js', array('vgc-slide-js', 'vgc-flexslider-js'), '20150320', false );
+        wp_enqueue_script( 'vgc-custommenu-js', TEMP_DIR . '/js/custommenu.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-ccard-js', TEMP_DIR . '/js/ccard.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-builder-js', TEMP_DIR . '/js/builder.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-dragdrop-js', TEMP_DIR . '/js/dragdrop.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-slider-js', TEMP_DIR . '/js/slider.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-form-js', TEMP_DIR . '/js/form.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-menu-js', TEMP_DIR . '/js/menu.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-translate-js', TEMP_DIR . '/js/translate.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-cookies-js', TEMP_DIR . '/js/cookies.js', array(), '20150320', false );
+        wp_enqueue_script( 'vgc-bootstrap-select.min-js', TEMP_DIR . '/js/bootstrap-select.min.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-jquery-ui-js', TEMP_DIR . '/js/jquery-ui.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-bootstrap-tooltip-js', TEMP_DIR . '/js/bootstrap-tooltip.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-ma.mobilemenu-js', TEMP_DIR . '/js/ma.mobilemenu.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-ma.menu.effect-js', TEMP_DIR . '/js/ma.menu.effect.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-backtotop-js', TEMP_DIR . '/js/backtotop.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-jquery.fancybox-js', TEMP_DIR . '/js/jquery.fancybox.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-likebox-js', TEMP_DIR . '/js/likebox.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-ma.script.vert-js', TEMP_DIR . '/js/ma.script.vert.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-ma.bxslider.min-js', TEMP_DIR . '/js/ma.bxslider.min.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-colorpicker-js', TEMP_DIR . '/js/colorpicker.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-jquery.cookie-js', TEMP_DIR . '/js/jquery.cookie.js', array(), '20150320', false );
+    //    wp_enqueue_script( 'vgc-ma.nivo.js-js', TEMP_DIR . '/js/ma.nivo.js.js', array(), '20150320', false );
+    }
+endif;
+
+add_action( 'wp_enqueue_scripts', 'vgc_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'vgc_enqueue_scripts' );
+
+
 /**
- * Enqueue scripts and styles.
- *
- * @since VGC 1.0
- */
-/*function vgc_scripts() {
-    // Add custom fonts, used in the main stylesheet.
-    wp_enqueue_style( 'vgc-fonts', vgc_fonts_url(), array(), null );
-
-    // Add ..., used in the main stylesheet.
-    // Load our main stylesheet.
-    wp_enqueue_style( 'vgc-style', get_stylesheet_uri() );
-    wp_enqueue_style( 'vgc-styles', get_template_directory_uri() . '/css/styles.css', array( 'vgc-style' ), '20150320' , 'all');
-
-    wp_enqueue_script( 'vgc-js-js', TEMP_DIR . '/js/js.js', array(), '20150320', false );
-}
-add_action( 'wp_enqueue_scripts', 'vgc_scripts' );*/
-
-function vgc_enqueue_style() {
-    // Add custom fonts, used in the main stylesheet.
-    wp_enqueue_style( 'vgc-fonts', vgc_fonts_url(), array(), null );
-
-    // Add ..., used in the main stylesheet.
-    // Load our main stylesheet.
-    wp_enqueue_style( 'vgc-core', get_stylesheet_uri(), false );
-    wp_enqueue_style( 'vgc-bootstrap', TEMP_DIR . '/css/bootstrap.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-styles', TEMP_DIR . '/css/styles.css', array( 'vgc-core' ), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-style', TEMP_DIR . '/css/style.css', array( 'vgc-core' ), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-bootstrap-select.min', TEMP_DIR . '/css/bootstrap-select.min.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-bootstrap-theme', TEMP_DIR . '/css/bootstrap-theme.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-widgets', TEMP_DIR . '/css/widgets.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-custommenu', TEMP_DIR . '/css/custommenu.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-effect', TEMP_DIR . '/css/effect.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-ma.onsaleslider', TEMP_DIR . '/css/ma.onsaleslider.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-categorytabs', TEMP_DIR . '/css/categorytabs.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-categorytabsliders', TEMP_DIR . '/css/categorytabsliders.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-searchbycategory', TEMP_DIR . '/css/searchbycategory.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-ma.cltool', TEMP_DIR . '/css/ma.cltool.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-ma.upsellslider', TEMP_DIR . '/css/ma.upsellslider.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-leftvmegamenu', TEMP_DIR . '/css/leftvmegamenu.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-ma.banner8', TEMP_DIR . '/css/ma.banner8.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-bannersequence', TEMP_DIR . '/css/bannersequence.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-ma.newslider', TEMP_DIR . '/css/ma.newslider.css', array(), '20150320' , 'all');
-    wp_enqueue_style( 'vgc-ajax_cart_super', TEMP_DIR . '/css/ajax_cart_super.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-jquery-ui', TEMP_DIR . '/css/jquery-ui.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-jquery.fancybox', TEMP_DIR . '/css/jquery.fancybox.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-bestsellervertscroller', TEMP_DIR . '/css/bestsellervertscroller.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-ma.brandslider.css', TEMP_DIR . '/css/ma.brandslider.css', array(), '20150320' , 'all');
-//    wp_enqueue_style( 'vgc-print.css', TEMP_DIR . '/css/print.css', array(), '20150320' , 'print');
-}
-
-function vgc_enqueue_script() {
-    wp_enqueue_script( 'vgc-jquery-js', '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js', array(), '20150320', false );
-    wp_enqueue_script( 'vgc-bootstrap', TEMP_DIR . '/js/bootstrap.js', array(), '20150320', false );
-    wp_enqueue_script( 'vgc-prototype-js', TEMP_DIR . '/js/prototype.js', array('vgc-jquery-js'), '20150320', false );
-    wp_enqueue_script( 'vgc-validation-js', TEMP_DIR . '/js/validation.js', array(), '20150320', false );
-    wp_enqueue_script( 'vgc-effects-js', TEMP_DIR . '/js/effects.js', array(), '20150320', false );
-    wp_enqueue_script( 'vgc-controls-js', TEMP_DIR . '/js/controls.js', array('vgc-prototype-js','vgc-effects-js'), '20150320', false );
-    wp_enqueue_script( 'vgc-js-js', TEMP_DIR . '/js/js.js', array('vgc-prototype-js', 'vgc-validation-js', 'vgc-controls-js'), '20150320', false );
-    wp_enqueue_script( 'vgc-slide-js', TEMP_DIR . '/js/ma.jq.slide.js', array(), '20150320', false );
-    wp_enqueue_script( 'vgc-flexslider-js', TEMP_DIR . '/js/ma.flexslider.js', array(), '20150320', false );
-    wp_enqueue_script( 'vgc-sequence-js', TEMP_DIR . '/js/jquery.sequence-min.js', array('vgc-slide-js', 'vgc-flexslider-js'), '20150320', false );
-    wp_enqueue_script( 'vgc-custommenu-js', TEMP_DIR . '/js/custommenu.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-ccard-js', TEMP_DIR . '/js/ccard.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-builder-js', TEMP_DIR . '/js/builder.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-dragdrop-js', TEMP_DIR . '/js/dragdrop.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-slider-js', TEMP_DIR . '/js/slider.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-form-js', TEMP_DIR . '/js/form.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-menu-js', TEMP_DIR . '/js/menu.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-translate-js', TEMP_DIR . '/js/translate.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-cookies-js', TEMP_DIR . '/js/cookies.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-bootstrap-select.min-js', TEMP_DIR . '/js/bootstrap-select.min.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-jquery-ui-js', TEMP_DIR . '/js/jquery-ui.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-bootstrap-tooltip-js', TEMP_DIR . '/js/bootstrap-tooltip.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-ma.mobilemenu-js', TEMP_DIR . '/js/ma.mobilemenu.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-ma.menu.effect-js', TEMP_DIR . '/js/ma.menu.effect.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-backtotop-js', TEMP_DIR . '/js/backtotop.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-jquery.fancybox-js', TEMP_DIR . '/js/jquery.fancybox.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-likebox-js', TEMP_DIR . '/js/likebox.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-ma.script.vert-js', TEMP_DIR . '/js/ma.script.vert.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-ma.bxslider.min-js', TEMP_DIR . '/js/ma.bxslider.min.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-colorpicker-js', TEMP_DIR . '/js/colorpicker.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-jquery.cookie-js', TEMP_DIR . '/js/jquery.cookie.js', array(), '20150320', false );
-//    wp_enqueue_script( 'vgc-ma.nivo.js-js', TEMP_DIR . '/js/ma.nivo.js.js', array(), '20150320', false );
-}
-
-add_action( 'wp_enqueue_scripts', 'vgc_enqueue_style' );
-add_action( 'wp_enqueue_scripts', 'vgc_enqueue_script' );
+ * Add a `screen-reader-text` class to the search form's submit button.
--- wp-content/themes/VGC/header.php
+++ wp-content/themes/VGC/header.php
@@ -66,59 +66,7 @@
                             <div class="col-xs-12 col-md-6 col-sm-12">
                                 <div class="header-box">
                                     <div class="search-container">
-
-                                        <form id="search_mini_form" action="" method="get">
-                                            <div class="form-search">
-                                                <label for="search">Search:</label>
-                                                <div class="box-select">
-                                                    <div class="header-select">
-                                                        <select class="selectpicker" id="cat" name="cat" style="display: none;">
-                                                            <option value="">Categories</option>
-                                                            <option value="3">Bracelets</option>
-                                                            <option value="4">Earrings</option>
-                                                            <option value="5">Necklaces</option>
-                                                            <option value="6">Rings</option>
-                                                            <option value="7">Watches</option>
-                                                            <option value="8">Wedding</option>
-                                                        </select>
-                                                        <div class="btn-group bootstrap-select">
-                                                            <button type="button" class="btn dropdown-toggle selectpicker btn-default" data-toggle="dropdown" data-id="cat" title="Categories">
-                                                                <span class="filter-option pull-left">Categories</span>&nbsp;<span class="caret"></span>
-                                                            </button>
-                                                            <div class="dropdown-menu open">
-                                                                <ul class="dropdown-menu inner selectpicker" role="menu">
-                                                                    <li rel="0" class="selected"><a tabindex="0" class="" style=""><span class="text">Categories</span></a>
-                                                                    </li>
-                                                                    <li rel="1"><a tabindex="0" class="" style=""><span class="text">Bracelets</span></a>
-                                                                    </li>
-                                                                    <li rel="2"><a tabindex="0" class="" style=""><span class="text">Earrings</span></a>
-                                                                    </li>
-                                                                    <li rel="3"><a tabindex="0" class="" style=""><span class="text">Necklaces</span></a>
-                                                                    </li>
-                                                                    <li rel="4"><a tabindex="0" class="" style=""><span class="text">Rings</span></a>
-                                                                    </li>
-                                                                    <li rel="5"><a tabindex="0" class="" style=""><span class="text">Watches</span></a>
-                                                                    </li>
-                                                                    <li rel="6"><a tabindex="0" class="" style=""><span class="text">Wedding</span></a>
-                                                                    </li>
-                                                                </ul>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="header-input">
-                                                    <input id="search" type="text" name="q" class="input-text" autocomplete="off">
-                                                </div>
-                                                <button type="submit" title="Search" class="button"><span><span>Search</span></span></button>
-
-                                            </div>
-                                        </form>
-                                        <script type="text/javascript">
-                                            //<![CDATA[
-                                            var searchForm = new Varien.searchForm('search_mini_form', 'search', 'Search entire store here...');
-                                            searchForm.initAutocomplete('', 'search_autocomplete');
-                                            //]]>
-                                        </script>
+                                        <?php get_search_form(  ); ?>
                                     </div>
 
                                 </div>
