9b59495052c22d879eb8fcdabf9f2f07d0db7c50
lamquoctuan <lamquoctuan@gmail.com>
2015-03-25 18:09:26 -0500

    about pagee template

---
+++ wp-content/themes/VGC/templates/pages/about.php
@@ -0,0 +1,20 @@
+<div class="breadcrumbs">
+    <ul>
+        <li class="home"><a href="<?php echo site_url();?>" title="Go to Home Page">Home</a><span>/</span></li>
+        <li class="cms_page"><?php the_title('<strong>','</strong>')?></li>
+    </ul>
+</div>
+<div class="std">
+    <div class="page-title mb20">
+        <h1>About Vancouver Gold for Cash</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-sms-6 col-smb-12">
+            <img src="<?php echo TEMP_URI."/images/about.jpg"?>" />
+        </div>
+        <div class="col-md-8 col-sms-6 col-smb-12">
+            <p>Vancouver Gold for Cash is the most trusted gold buyer in Greater Vancouver Area. With 20 years of experience in refining precious metal industry, our professional will guide you the best ways to sell gold, silver and other precious metals such as platinum and palladium.</p>
+        </div>
+
+    </div>
+</div>
