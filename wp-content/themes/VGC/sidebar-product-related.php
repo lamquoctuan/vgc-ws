23e1b40e2cfee0e2614ed4c3a9514df96598ea79
lamquoctuan <lamquoctuan@gmail.com>
2015-04-08 09:49:59 -0500

    load dynamic other products

--- wp-content/themes/VGC/sidebar-product-related.php
+++ wp-content/themes/VGC/sidebar-product-related.php
@@ -6,9 +6,52 @@
         </div>
         <div class="featured-products">
             <ul class="products-grid  first last odd">
-                <li class="item first">
+                <?php
+                $args = array(
+                    'posts_per_page'   => 3,
+                    'orderby'          => 'post_date',
+                    'order'            => 'DESC',
+                    'exclude'          => $post->ID,
+                    'post_type'        => $post->post_type,
+                    'post_status'      => 'publish'
+                );
+                $posts_array = get_posts( $args );
+                $idx = 0;
+                foreach ( $posts_array as $post ) : setup_postdata( $post );
+                    $post_featured_image = get_post_thumbnail_id();
+                    $classLi = ($idx == 0)?'item first':($idx == count($posts_array)-1?'item last':'item');
+                    $sku = intval(get_field('sku'));
+                    $unitPrice = intval(get_field('unit_price'));
+                    $salePrice = intval(get_field('sale_price'));
+                    $unitPriceFormatted = number_format($unitPrice, 2);
+                    $salePriceFormatted = number_format($salePrice, 2);
+                    ?>
+                    <li class="<?php echo $classLi;?>">
+                        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="product-image">
+                            <?php the_post_thumbnail('product-small');?>
+                        </a>
+                        <div class="box-feature">
+                            <h2 class="product-name"><a href="" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                            <div class="price-box">
+                                <p class="special-price">
+                                    <span class="price-label">Special Price</span>
+                                    <span class="price" id="product-price-1"><?php echo $salePriceFormatted;?></span>
+                                </p>
+                                <p class="old-price">
+                                    <span class="price-label">Regular Price:</span>
+                                    <span class="price" id="old-price-1"><?php echo $unitPriceFormatted;?></span>
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                <?php
+                $idx ++;
+                endforeach;
+                wp_reset_postdata();
+                ?>
+               <!-- <li class="item first">
                     <a href="#" title="Fusce aliquam" class="product-image">
-                        <img src="<?php echo TEMP_URI;?>/assets/images/best/1.jpg" width="500" height="500" alt="Jewelry Name">
+                        <img src="<?php /*echo TEMP_URI;*/?>/assets/images/best/1.jpg" width="500" height="500" alt="Jewelry Name">
                     </a>
                     <div class="box-feature">
                         <h2 class="product-name"><a href="" title="Jewelry Name">Jewelry Name</a></h2>
@@ -24,10 +67,9 @@
                         </div>
                     </div>
                 </li>
-                <!--  -->
                 <li class="item">
                     <a href="#" title="Proin lectus ipsum" class="product-image">
-                        <img src="<?php echo TEMP_URI;?>/assets/images/best/2.jpg" width="500" height="500" alt="Jewelry Name">
+                        <img src="<?php /*echo TEMP_URI;*/?>/assets/images/best/2.jpg" width="500" height="500" alt="Jewelry Name">
                     </a>
                     <div class="box-feature">
                         <h2 class="product-name"><a href="#" title="Jewelry Name">Jewelry Name</a></h2>
@@ -43,10 +85,9 @@
                         </div>
                     </div>
                 </li>
-                <!--  -->
                 <li class="item last">
                     <a href="#" title="Quisque in arcu" class="product-image">
-                        <img src="<?php echo TEMP_URI;?>/assets/images/best/3.jpg" width="500" height="500" alt="Jewelry Name">
+                        <img src="<?php /*echo TEMP_URI;*/?>/assets/images/best/3.jpg" width="500" height="500" alt="Jewelry Name">
                     </a>
                     <div class="box-feature">
                         <h2 class="product-name"><a href="#" title="Jewelry Name">Jewelry Name</a></h2>
@@ -61,7 +102,7 @@
                             </p>
                         </div>
                     </div>
-                </li>
+                </li>-->
             </ul>
 
         </div>
